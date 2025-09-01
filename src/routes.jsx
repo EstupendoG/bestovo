eae127c0eea9598c98807224457844b84d74abe8
unknown <rafagonpin317@gmail.com>
2025-08-31 22:20:27 -0300

    Resolucao de erros no deploy

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,16 +1,16 @@
 import React from "react";
-import { Route, Routes, BrowserRouter } from "react-router-dom";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import Homepage from "./pages/Homepage/Homepage";
 import Portfolio from "./pages/Portfolio/Portfolio";
 
 const AppRoutes = () => {
     return(
-        <BrowserRouter>
+        <Router>
             <Routes>
-                <Route element={<Homepage/>} path="/" />
+                <Route element={<Homepage/>} path="" />
                 <Route element={<Portfolio/>} path="/portfolio" />
             </Routes>
-        </BrowserRouter>
+        </Router>
     )
 }
 
